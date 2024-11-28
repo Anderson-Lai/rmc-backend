eaa5bd9c43a609e1cd6edd719f5fdb2ebfa0750c
Anderson-Lai <andersonmhlai26@gmail.com>
2024-11-27 21:36:31 -0500

    readd vercel.json

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response, Express } from "express";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 function main() {
-    const app: Express = express();
+    const app = express();
     const port = Number(process.env.PORT) || 2957;
 
     app.use(express.json());
@@ -12,11 +12,11 @@ function main() {
         console.log(`Server is running on port ${port}`);
     });
 
-    app.get("/test", (_req: Request, res: Response) => {
+    app.get("/", (_req, res) => {
         res.status(200).json({
             response: "hello world!"
         });
-    });
+    })
 }
 
 main();
