3539b984f34e04742edbf53e9bff93c6716954f7
Anderson-Lai <andersonmhlai26@gmail.com>
2024-11-28 12:52:32 -0500

    refactoring

--- api/index.ts
+++ api/index.ts
@@ -24,28 +24,31 @@ function main() {
 
     app.post("/form", rateLimiter, async (req, res) => {
         try {
-            const { producerEmail, eventName, eventDate, eventSignificance, eventPortrayal, mediaBreakdown } = req.body;
+            const { producerEmail, eventName, eventDate, 
+                eventSignificance, eventPortrayal, mediaBreakdown } = req.body;
+
             if (!producerEmail || !eventName || !eventDate || !eventSignificance
                 || !eventPortrayal || !mediaBreakdown) {
                 res.sendStatus(400);
                 return;
             }
 
-            const emailer = new Email(`RMC Automatic Emailing <${process.env.EMAIL_USERNAME}`,
-                [process.env.EMAIL_RECIPIENT_USERNAME || ""],
-                `Pitch Proposal for '${eventName}' by ${producerEmail}`,
-                `A new form submission has been completed on the website with the following details:\n\
+            const username = process.env.EMAIL_USERNAME || "";
+            const password = process.env.EMAIL_PASSWORD || "";
+            const recipient = process.env.EMAIL_RECIPIENT_USERNAME || "";
+            const fromHeading = `RMC Automatic Emailing <${username}>`;
+            const recipients = [recipient];
+            const subject = `Pitch Proposal for ${eventName} by ${producerEmail}`;
+            const text = `A new form submission has been completed on the website with the following details:\n\
                 Email of producer: ${producerEmail}\n\
                 Name of event: ${eventName}\n\
                 Date of event: ${eventDate}\n\
                 Event significance: ${eventSignificance}\n\
                 Event portrayal: ${eventPortrayal}\n\
-                Rough breakdown: ${mediaBreakdown}\n`,
-                process.env.EMAIL_USERNAME || "",
-                process.env.EMAIL_PASSWORD || "",
-                "smtp.gmail.com"
-            )
+                Rough breakdown: ${mediaBreakdown}\n`;
+            const smtp = "smtp.gmail.com";
 
+            const emailer = new Email(fromHeading, recipients, subject, text, username, password, smtp);
             await emailer.sendEmail();
 
             res.sendStatus(200);
