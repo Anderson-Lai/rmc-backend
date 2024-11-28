b34c9acb84f51730bed5162d1c598c71fa2c87ac
Anderson-Lai <andersonmhlai26@gmail.com>
2024-11-27 21:26:41 -0500

    fix type issues

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,21 @@
-import express from "express";
+import express, { Request, Response, Express } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 function main() {
-    const app = express();
+    const app: Express = express();
     const port = Number(process.env.PORT) || 2957;
 
     app.use(express.json());
 
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
+    });
+
+    app.get("/test", (_req: Request, res: Response) => {
+        res.status(200).json({
+            response: "hello world!"
+        });
     });
 }
 
