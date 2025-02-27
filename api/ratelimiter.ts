e863084868bc4a68892997d9a61efb0424a3e90b
Anderson-Lai <andersonmhlai26@gmail.com>
2025-02-27 09:44:36 -0500

    update rate limiting

--- api/ratelimiter.ts
+++ api/ratelimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
     windowMs: 1000 * 60 * 60 * 24, // daily
-    limit: 95, // max requests per day to prevent malicious actors
+    limit: 24, // max requests per day to prevent malicious actors
     message: "You have the maximum requests per day",
     standardHeaders: true,
     legacyHeaders: true,
